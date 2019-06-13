48ee0e2b3aa3c29fecf45cae2c53236465842961
allan baruiz <allan_baruiz@ligph.com>
2019-06-13 16:51:56 +0800

    Adding Database, Create and Show  data

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,22 +1,84 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Post;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PostController extends Controller
 {
 	/**
-	 * @Route("/")
-	 * @Method({"GET"})
+	 * @Route("/", name="index")
+	 * @Method({"GET", "POST"})
 	 */
     public function index()
     {
-        return $this->render('posts/index.html.twig', array(
-        	'post' => 'Hello world!' ,
-    		'description' => 'Welcome to the outside world!' 
-        ));
+        $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
+
+        return $this->render('posts/index.html.twig', array('posts' => $posts));
+    }
+
+    /**
+     * @Route("/posts/{id}")
+     * @Method({"GET"})
+     */
+    public function show($id)
+    {
+        $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
+        
+        return $this->render('posts/show.html.twig', array('post' => $post ));
+    }
+
+    /**
+     * @Route("/posts")
+     * @Method({"GET", "POST"})
+     */
+    public function store(Request $request)
+    {
+        $post = new Post();
+
+        $form = $this->createFormBuilder($post)
+            ->add('title', TextType::class, array('attr' =>
+                array('class' => 'form-control')
+            ))
+            ->add('description', TextareaType::class, array(
+                'attr' =>
+                array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Create',
+                'attr' => array('class' => 'btn btn-info btn-block mt-3')
+            ))->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $post = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('posts/create.html.twig', array('form' => $form->createView() ));
+    }
+
+    /**
+     * @Route("/posts/{id}/edit")
+     * @Method({"GET"})
+     */
+    public function edit($id)
+    {
+        $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
+        
+        return $this->render('posts/edit.html.twig', array('post' => $post ));
     }
 }
